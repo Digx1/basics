0b3afc016705d4e00096c415eea1dd41e7e1facd
Digvijay <slayerpatil17@gmail.com>
2025-01-18 15:10:01 +0530

    created user entity

--- basics/src/main/java/com/study/basics/entity/User.java
+++ basics/src/main/java/com/study/basics/entity/User.java
@@ -3,7 +3,11 @@ package com.study.basics.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
