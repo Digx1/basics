8145077f056e9bfc4f67985844717fcebe055e19
Digvijay <slayerpatil17@gmail.com>
2025-01-23 00:11:29 +0530

    axz

--- basics/src/main/java/com/study/basics/controller/UserController.java
+++ basics/src/main/java/com/study/basics/controller/UserController.java
@@ -2,6 +2,7 @@ package com.study.basics.controller;
 
 import com.study.basics.dto.UserDTO;
 import com.study.basics.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Transactional
 @RequestMapping("api/user")
 public class UserController {
 
@@ -28,13 +32,14 @@ public class UserController {
 
 
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(UserDTO userDTO) {
+    @Transactional
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         UserDTO savedUser = userService.createUser(userDTO);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UserDTO> getUserById(Long id) {
+    public ResponseEntity<UserDTO> getUserById(@PathVariable(value = "id") Long id) {
         UserDTO user = userService.getUserById(id);
         return ResponseEntity.ok(user);
 
